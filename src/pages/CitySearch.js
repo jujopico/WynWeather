41644f990bb3e85339f7ba1086fdd602e00243d0
jujopico <sujunqt@gmail.com>
2020-02-11 15:51:52 -0500

    single city

--- src/pages/CitySearch.js
+++ src/pages/CitySearch.js
@@ -9,29 +9,28 @@ class CitySearch extends Component {
         .then(response => response.json())
         .then(data => this.setState({ cities: data }))
     }
-    
-    render(){
-    return(
-      <div>
-        <h3>City Search</h3>
-        <input
-          type="text"
-          autoComplete="off"
-          onChange={this.handleCitySearch} 
-        />
-        {
-          this.state.cities.map(city => (
-            <Link key={city.id} to={`/cities/${city.id}`}>
-              <div className="citySearch">
-                <h3>{city.name}</h3>
-              </div>
-            </Link>
-          ))
-        }
-      </div>
 
-    )
+    render(){
+      return(
+        <div>
+          <h3>City Search</h3>
+          <input
+            type="text"
+            autoComplete="off"
+            onChange={this.handleCitySearch} 
+          />
+          {
+            this.state.cities.map(city => (
+              <Link key={city.id} to={`/cities/${city.id}`}>
+                <div className="citySearch">
+                  <h3>{city.name}</h3>
+                </div>
+              </Link>
+            ))
+          }
+        </div>
+      )
+    }
   }
-}
 
 export default CitySearch
--- src/pages/SingleCity.js
+++ src/pages/SingleCity.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
 
 class SingleCity extends Component {
+  state = { 
+    city: '',
+    forecasts: []
+  }
+
+ handleSingleCity = () => {
+    fetch(`https://wyn-weather-api.herokuapp.com/cities/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(data => this.setState({ city: data.name, forecasts: data.forecasts }))
+  }
+
+  componentDidMount(){
+    this.handleSingleCity()}
+
   render(){
     return(
-      <h1>Single City</h1>
+      <>
+      <h3>{this.state.city.name}</h3>
+      <div>
+       { this.state.forecasts.map(day => (
+         <>
+         <h4>{day.date}</h4>
+         <h5>{day.weather}</h5>
+         <img src={day.image} alt={day.weather} />
+         <h5>Low: {day.min_temp}</h5>
+         <h5>High: {day.max_temp}</h5>
+         <h5>Humidity: {day.humidity}</h5>
+         </>
+       ))}
+      </div>
+      </>
     )
   }
 }
